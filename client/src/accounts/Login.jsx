c5f0ea1966650e2306195335147a3bdedec760bd
Sri karthick <srikarthi01234@gmail.com>
2022-08-01 18:05:08 +0530

    Formatted code

--- client/src/accounts/Login.jsx
+++ client/src/accounts/Login.jsx
@@ -1,58 +1,63 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
+import Category from "../core/category";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+  const emailChange = (event) => {
+    setEmail(
+      event.target.value.replace(/"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"/, "")
+    );
+  };
 
-    const emailChange = (event) =>{
-        setEmail(event.target.value.replace(/"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"/,
-        ""))
+  const passwordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const onSubmitLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const body = { email, password };
+      const response = await fetch("http://localhost:4000/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      const result = await response.json();
+      console.log(result);
+      result === true
+        ? (window.location = "/post/product")
+        : alert("Login credentials are wrong!!!");
+    } catch (err) {
+      console.error(err.message);
     }
-
-    const passwordChange = (event) =>{
-        setPassword(event.target.value)
-    }
-
-    const onSubmitLogin = async(e) => {
-        e.preventDefault();
-
-        try {
-            const body = {email, password}
-            const response = await fetch("http://localhost:4000/auth/login",{
-                method:"POST",
-                headers:{ "Content-Type" : "application/json" },
-                body: JSON.stringify(body)
-            })
-            const result = await response.json();
-            console.log(result);
-            result === true ? window.location = "/post/product" : alert("Login credentials are wrong!!!")
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
+  };
 
   return (
     <Fragment>
       <form onSubmit={onSubmitLogin}>
         <h1>Login</h1>
         <div>
-          <input 
-          type="text" 
-          placeholder="email" 
-          value={email}
-          onChange={emailChange}
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={emailChange}
           />
-        </div><br />
+        </div>
+        <br />
         <div>
-          <input 
-          type="password" 
-          placeholder="password" 
-          value={password}
-          onChange={passwordChange}
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={passwordChange}
           />
-        </div><br />
+        </div>
+        <br />
         <div>
           <button type="submit">Submit</button>
         </div>
