6deca782f2c5e11fcd315215cbdaa63955afc6a5
Sri karthick <srikarthi01234@gmail.com>
2022-08-06 12:06:05 +0530

    Added Rendering of products in table format

--- client/src/core/products.jsx
+++ client/src/core/products.jsx
@@ -1,31 +1,63 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const { category } = useParams();
+  const { uid } = useParams();
+  const { deliverylocation } = useParams();
 
-    const {category} = useParams(); 
-    const {deliverylocation} = useParams(); 
-
-    const renderProducts = async() =>{
-        try {
-            const response = await fetch(`http://localhost:4000/products/${category}/${deliverylocation}`);
-            const jsonData = await response.json();
-            console.log(jsonData)
-        } catch (err) {
-            console.error(err.message);
-        }
+  const renderProducts = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/products/${category}/${deliverylocation}`
+      );
+      const jsonData = await response.json();
+      setProducts(jsonData);
+    } catch (err) {
+      console.error(err.message);
     }
+  };
 
-    useEffect(()=>{
-        renderProducts();
-    })
+  useEffect(() => {
+    renderProducts();
+  }, []);
 
-    return(
-        <Fragment>
-            
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <center>
+        <table>
+          <thead>
+            <tr>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>Delivery Charge</th>
+              <th>Delivery location</th>
+              <th>Total Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((prod) => (
+              <tr
+                key={prod.id}
+                onClick={() => {
+                  window.location = `/product/${prod.id}/${uid}`;
+                }}
+              >
+                <td>{prod.pname}</td>
+                <td>{prod.price}</td>
+                <td>{prod.deliverycharge}</td>
+                <td>{prod.deliverylocation}</td>
+                <td>{prod.price + prod.deliverycharge}</td>
+                <td></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </center>
+    </Fragment>
+  );
+};
 
 export default Products;
 
