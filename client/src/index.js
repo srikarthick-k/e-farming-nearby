86b1dc329781565b908da24100274f56c6f6c6a9
Sri karthick <srikarthi01234@gmail.com>
2022-08-06 12:07:06 +0530

    Added product and home route

--- client/src/index.js
+++ client/src/index.js
@@ -9,18 +9,22 @@ import Login from "./accounts/Login";
 import Postproduct from "./core/postproduct";
 import Category from "./core/category";
 import Products from "./core/products";
+import Product from "./core/product";
+import Index from "./home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={ <Login /> } />
+            <Route path="/" element={ <Index /> } />
+            <Route path="/login" element={ <Login /> } />
             <Route path="/nav" element={ <Navbar /> }/>
             <Route path="/register" element={ <Register /> } />
-            <Route path="/post/product" element={ < Postproduct />}/>
-            <Route path="/category/:deliverylocation" element={ < Category />}  />
-            <Route path="/products/:category/:deliverylocation" element={ < Products /> }/>
+            <Route path="/category/:deliverylocation/:uid" element={ < Category />}  />
+            <Route path="/post/product/:uid" element={ < Postproduct />}/>
+            <Route path="/products/:category/:deliverylocation/:uid" element={ < Products /> }/>
+            <Route path="/product/:pid/:uid" element={ <Product /> }/>
         </Routes>
     </BrowserRouter>
   </React.StrictMode>
