f5f53a3da1fcbe1c17df41e50490f7610bc88eec
Sri karthick <srikarthi01234@gmail.com>
2022-08-07 21:58:06 +0530

    Added MUI Theme

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Components
 import Register from "./accounts/Register";
-import Navbar from "./components/Navbar";
 import Login from "./accounts/Login";
 import Postproduct from "./core/postproduct";
 import Category from "./core/category";
@@ -16,22 +15,42 @@ import Index from "./home";
 import FromOrder from "./core/orders/from-orders";
 import ToOrder from "./core/orders/to-orders";
 
+// Theme
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={ <Index /> } />
-            <Route path="/login" element={ <Login /> } />
-            <Route path="/nav" element={ <Navbar /> }/>
-            <Route path="/register" element={ <Register /> } />
-            <Route path="/category/:deliverylocation/:uid" element={ < Category />}  />
-            <Route path="/post/product/:uid" element={ < Postproduct />}/>
-            <Route path="/products/:category/:deliverylocation/:uid" element={ < Products /> }/>
-            <Route path="/product/:pid/:uid" element={ <Product /> }/>
-            <Route path="/from-orders/:uid" element={ < FromOrder/>} />
-            <Route path="/to-orders/:uid" element={ < ToOrder/>} />
-        </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            
+            <Route
+              path="/category/:deliverylocation/:uid"
+              element={<Category />}
+            />
+            <Route path="/post/product/:uid" element={<Postproduct />} />
+            <Route
+              path="/products/:category/:deliverylocation/:uid"
+              element={<Products />}
+            />
+            <Route path="/product/:pid/:uid" element={<Product />} />
+            
+            <Route path="/from-orders/:uid" element={<FromOrder />} />
+            <Route path="/to-orders/:uid" element={<ToOrder />} />
+          </Routes>
+        </BrowserRouter>
+      </CssBaseline>
+    </ThemeProvider>
   </React.StrictMode>
 );
