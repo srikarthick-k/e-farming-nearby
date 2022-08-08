34882a02e75936ca29fcf9553378ffa054eadcf5
Sri karthick <srikarthi01234@gmail.com>
2022-08-08 19:04:20 +0530

    Added MUI and default location func

--- client/src/core/orders/to-orders.jsx
+++ client/src/core/orders/to-orders.jsx
@@ -4,8 +4,21 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../../components/Navbar";
+// CSS
+import "../../style.css";
+// MUI
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
+
 const ToOrder = () => {
   const [orders, setOrders] = useState([]);
+  const [deliverylocation, setDeliveryLocation] = useState("");
   const { uid } = useParams();
 
   const onRender = async () => {
@@ -18,42 +31,72 @@ const ToOrder = () => {
     }
   };
 
+  const getDefaultCity = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/userinfo/${uid}`);
+      const jsonData = await response.json();
+      setDeliveryLocation(jsonData[0].city);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const delivered = async (orderid) => {
+    try {
+      await fetch(`http://localhost:4000/deleteorder/${orderid}`, {
+        method: "DELETE",
+      });
+    } catch (err) {
+      console.error(err.message);
+    }
+    setOrders(orders.filter((order) => order.orderid !== orderid));
+  };
+
   useEffect(() => {
     onRender();
+    getDefaultCity();
   }, []);
 
   return (
     <Fragment>
       <center>
-        <h1>To Orders</h1>
+        <Navbar uid={uid} deliverylocation={deliverylocation} />
+        <h1>Orders To You</h1>
         {orders[0] ? (
-          <table>
-            <thead>
-              <tr>
-                <th>Customer-Name |</th>
-                <th>Product-Name |</th>
-                <th>Quantity |</th>
-                <th>Total-Price |</th>
-                <th>City |</th>
-                <th>Address</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order) => (
-                <tr key={order.productid}>
-                  <td>{order.cname}</td>
-                  <td>{order.pname}</td>
-                  <td>{order.quantity}</td>
-                  <td>{order.quantity * order.price + order.deliverycharge}</td>
-                  <td>{order.city}</td>
-                  <td>{order.address}</td>
-                  <td>
-                    <button>Update</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>CUSTOMER-NAME</TableCell>
+                  <TableCell>PRODUCT-NAME</TableCell>
+                  <TableCell>QUANTITY</TableCell>
+                  <TableCell>TOTAL-PRICE</TableCell>
+                  <TableCell>CITY</TableCell>
+                  <TableCell>ADDRESS</TableCell>
+                  <TableCell>DELIVERED</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {orders.map((order) => (
+                  <TableRow className="trow" key={order.productid}>
+                    <TableCell>{order.cname}</TableCell>
+                    <TableCell>{order.pname}</TableCell>
+                    <TableCell>{order.quantity}</TableCell>
+                    <TableCell>
+                      {order.quantity * order.price + order.deliverycharge}
+                    </TableCell>
+                    <TableCell>{order.city}</TableCell>
+                    <TableCell>{order.address}</TableCell>
+                    <TableCell>
+                      <Button onClick={() => delivered(order.orderid)}>
+                        DELIVERED
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         ) : (
           <h1>No orders shop more</h1>
         )}
