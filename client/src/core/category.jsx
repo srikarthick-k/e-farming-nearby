2586d807d0918494016ed211584e5626c805869b
Sri karthick <srikarthi01234@gmail.com>
2022-08-12 20:53:45 +0530

    Added local storage

--- client/src/core/category.jsx
+++ client/src/core/category.jsx
@@ -1,20 +1,20 @@
-//** @TODO On next version 2.0 of this app I'm gonna include the option where user can search for available categories in that location. When they select the option from list of locations, the url parameter changes as well
-
 import React, { Fragment, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Button } from "@mui/material";
 
+// CSS
+import "../style.css";
+
 function Category() {
-  const { deliverylocation } = useParams();
-  const { uid } = useParams();
+  const deliverylocation = localStorage.getItem("defaultDistrict");
   const [categories, setcategories] = useState([]);
 
   const onRender = async () => {
     try {
-      const response = await fetch(
-        `http://localhost:4000/category/${deliverylocation}`
-      );
+      const response = await fetch(`http://localhost:4000/category`, {
+        method: "GET",
+        headers: { defaultDistrict: deliverylocation },
+      });
       const jsonData = await response.json();
       setcategories(jsonData);
     } catch (err) {
@@ -28,25 +28,26 @@ function Category() {
 
   const CategoriesRender = () => {
     return (
-      <ul>
+      <ul className="cats">
         {categories.map((cat) => (
-          <Button
-            key={cat.category}
-            onClick={() =>
-              (window.location = `/products/${cat.category}/${deliverylocation}/${uid}`)
-            }
-            variant="outlined"
-          pill>
-            {cat.category}
-          </Button>
+          
+            <Button
+              key={cat.category}
+              onClick={() => (window.location = `/products/${cat.category}`)}
+              variant="outlined"
+              
+            >
+              {cat.category}
+            </Button>
         ))}
       </ul>
     );
   };
+
   return (
     <Fragment>
       <div>
-        <Navbar uid={uid} deliverylocation={deliverylocation}/>
+        <Navbar />
         <h1>Categories: </h1>
         {categories[0] ? (
           <CategoriesRender />
