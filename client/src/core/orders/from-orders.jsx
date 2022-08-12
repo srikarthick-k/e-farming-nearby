468a60cf5d88209ef49f8c654fbc278ba44ec8da
Sri karthick <srikarthi01234@gmail.com>
2022-08-12 20:54:49 +0530

    Added local storage

--- client/src/core/orders/from-orders.jsx
+++ client/src/core/orders/from-orders.jsx
@@ -6,23 +6,22 @@ import { useParams } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 
 // CSS
-import "../../style.css"
+import "../../style.css";
 
 // MUI
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import { Button } from "@mui/material";
-
 
 const FromOrder = () => {
   const [orders, setOrders] = useState([]);
-  const [deliverylocation, setDeliveryLocation] = useState("")
-  const { uid } = useParams();
+  const [deliverylocation, setDeliveryLocation] = useState("");
+  const uid = localStorage.getItem("uid");
 
   const onRender = async () => {
     try {
@@ -33,15 +32,15 @@ const FromOrder = () => {
       console.error(err.message);
     }
   };
-  const getDefaultCity = async() =>{
+  const getDefaultCity = async () => {
     try {
-      const response = await fetch(`http://localhost:4000/userinfo/${uid}`)
+      const response = await fetch(`http://localhost:4000/userinfo/${uid}`);
       const jsonData = await response.json();
-      setDeliveryLocation(jsonData[0].city)
+      setDeliveryLocation(jsonData[0].city);
     } catch (err) {
       console.error(err.message);
     }
-  }
+  };
   useEffect(() => {
     onRender();
     getDefaultCity();
@@ -50,45 +49,46 @@ const FromOrder = () => {
   return (
     <Fragment>
       <Navbar uid={uid} deliverylocation={deliverylocation} />
+      <center>
         <h1>Orders From You</h1>
 
         {orders[0] ? (
           <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>SELLER-NAME</TableCell>
-                <TableCell>PRODUCT-NAME</TableCell>
-                <TableCell>QUANTITY</TableCell>
-                <TableCell>TOTAL-PRICE</TableCell>
-                <TableCell>CITY</TableCell>
-                <TableCell>ADDRESS</TableCell>
-                <TableCell>UPDATE</TableCell>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>SELLER-NAME</TableCell>
+                  <TableCell>PRODUCT-NAME</TableCell>
+                  <TableCell>QUANTITY</TableCell>
+                  <TableCell>TOTAL-PRICE</TableCell>
+                  <TableCell>CITY</TableCell>
+                  <TableCell>ADDRESS</TableCell>
+                  <TableCell>UPDATE</TableCell>
                 </TableRow>
-          </TableHead>
-          <TableBody>
-              {orders.map((order) => (
-                <TableRow 
-                className="trow"
-                key={order.productid}
-                >
-                  <TableCell>{order.sname}</TableCell>
-                  <TableCell>{order.pname}</TableCell>
-                  <TableCell>{order.quantity}</TableCell>
-                  <TableCell>{order.quantity * order.price + order.deliverycharge}</TableCell>
-                  <TableCell>{order.city}</TableCell>
-                  <TableCell>{order.address}</TableCell>
-                  <TableCell>
-                    <Button>UPDATE</Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {orders.map((order) => (
+                  <TableRow className="trow" key={order.productid}>
+                    <TableCell>{order.sname}</TableCell>
+                    <TableCell>{order.pname}</TableCell>
+                    <TableCell>{order.quantity}</TableCell>
+                    <TableCell>
+                      {order.quantity * order.price + order.deliverycharge}
+                    </TableCell>
+                    <TableCell>{order.city}</TableCell>
+                    <TableCell>{order.address}</TableCell>
+                    <TableCell>
+                      <Button>UPDATE</Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         ) : (
           <h1>No orders shop more</h1>
         )}
+      </center>
     </Fragment>
   );
 };
